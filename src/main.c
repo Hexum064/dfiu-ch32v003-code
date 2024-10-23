dc5558813cbedddc8cf7163d10c670c5b4c009ff
hexum064 <hexum064@gmail.com>
2024-10-22 18:20:54 -0700

    display lights added

--- src/main.c
+++ src/main.c
@@ -13,18 +13,19 @@
 
 #define AUDIO_EN	PA1
 
-#define PWM_OUT		PC4
+#define PWM_OUT		PD0
 
-#define TOUCH_P		PA1
+#define TOUCH_P		PD2
 
-#define LED_0		PD0
-#define LED_1		PD1
-#define LED_2		PD2
-#define LED_3		PD3
-#define LED_4		PD4
-#define LED_5		PD5
-#define LED_6		PD6
-#define LED_7		PD7
+#define DISP_LEDS_P GPIOC
+#define LED_0		PC0
+#define LED_1		PC1
+#define LED_2		PC2
+#define LED_3		PC3
+#define LED_4		PC4
+#define LED_5		PC5
+#define LED_6		PC6
+#define LED_7		PC7
 
 #define SAMPLE_DELAY 6000		// 48MHz / 6000 = 8KHz
 #define TOUCH_THRESHHOLD 7000	// Has to be over this to register as a touch
@@ -42,11 +43,27 @@
 
 #define LED_U_ON (LED_I_OFF + 200)
 #define LED_U_OFF (AUDIO_SIZE - 100)
-#define DEBUG
+// #define DEBUG
 
 uint16_t idx = 0;
 uint8_t button_down = 0;
 uint8_t audio_loop_delaying = 0;
+uint8_t display_led_reg = 0x7F;
+
+#define DISP_INIT_VAL_LEN 8
+#define DISP_UPDATE 5
+#define DISP_UPDATE_NEXT_INIT 87
+
+uint8_t display_init_vals[] = {
+	0x01,
+	0x55,
+	0x7F,
+	0x70,
+	0x11,
+	0x0F,
+	0x33,
+	0x03
+};
 
 void all_dfiu_leds_off()
 {
@@ -128,11 +145,11 @@ void audio_update()
 		TIM2->INTFR = TIM_CC1IF;
 		if (idx >= AUDIO_SIZE)
 		{
-			TIM1->CH4CVR = audio_dfiu[AUDIO_SIZE - 1];
+			TIM1->CH1CVR = audio_dfiu[AUDIO_SIZE - 1];
 		}
 		else
 		{
-			TIM1->CH4CVR = audio_dfiu[idx];
+			TIM1->CH1CVR = audio_dfiu[idx];
 		}
 	}
 }
@@ -153,11 +170,19 @@ void gpios_init()
 	funPinMode(LED_F, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP); // F LED
 	funPinMode(LED_I, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP); // I LED
 	funPinMode(LED_U, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP); // U LED
-	funPinMode(PWM_OUT, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF); 
+	funPinMode(PWM_OUT, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF); //PWM Pin
+	funPinMode(LED_0, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP); // display LED
+	funPinMode(LED_1, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP); // display LED
+	funPinMode(LED_2, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP); // display LED
+	funPinMode(LED_3, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP); // display LED
+	funPinMode(LED_4, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP); // display LED
+	funPinMode(LED_5, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP); // display LED
+	funPinMode(LED_6, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP); // display LED
+	funPinMode(LED_7, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP); // display LED
 
 	// PC4 is T1CH4, 10MHz Output alt func, push-pull
-	// GPIOC->CFGLR &= ~(0xf << (4 * 4));s
-	// GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF) << (4 * 4);
+	// GPIOA->CFGLR &= ~(0xf << (4 * PWM_OUT));
+	// GPIOA->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF) << (4 * PWM_OUT);
 }
 
 void pwm_init()
@@ -183,20 +208,20 @@ void pwm_init()
 	TIM1->SWEVGR |= TIM_UG;
 
 	// Enable CH1N output, positive pol
-	// TIM1->CCER |= TIM_CC1NE | TIM_CC1NP;
+	TIM1->CCER |= TIM_CC1NE | TIM_CC1NP;
 
 	// Enable CH4 output, positive pol
-	TIM1->CCER |= TIM_CC4E | TIM_CC4P;
+	//TIM1->CCER |= TIM_CC2E | TIM_CC2P;
 
 	// CH1 Mode is output, PWM1 (CC1S = 00, OC1M = 110)
-	// TIM1->CHCTLR1 |= TIM_OC1M_2 | TIM_OC1M_1;
+	TIM1->CHCTLR1 |= TIM_OC1M_2 | TIM_OC1M_1;
 
 	// CH2 Mode is output, PWM1 (CC1S = 00, OC1M = 110)
-	TIM1->CHCTLR2 |= TIM_OC4M_2 | TIM_OC4M_1;
+	//TIM1->CHCTLR1 |= TIM_OC2M_2 | TIM_OC2M_1;
 
 	// Set the Capture Compare Register value to 50% initially
-	// TIM1->CH1CVR = 128;
-	TIM1->CH4CVR = 128;
+	 TIM1->CH1CVR = 128;
+	//TIM1->CH2CVR = 128;
 
 	// Enable TIM1 outputs
 	TIM1->BDTR |= TIM_MOE;
@@ -222,7 +247,7 @@ void sample_timer_init()
 void sao_init()
 {
 	// Enable GPIOs and timers
-	RCC->APB2PCENR |= RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOC | RCC_APB2Periph_TIM1 | RCC_APB2Periph_ADC1;
+	RCC->APB2PCENR |= RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD | RCC_APB2Periph_TIM1 | RCC_APB2Periph_ADC1;
 	RCC->APB1PCENR |= RCC_APB1Periph_TIM2;
 
 	gpios_init();
@@ -233,6 +258,15 @@ void sao_init()
 
 	// turn on audio disable pin. disabled high
 	funDigitalWrite(AUDIO_EN, FUN_HIGH);
+}
+
+void display_leds_update()
+{
+	uint8_t temp = display_led_reg & 0x01;
+	display_led_reg >>= 1;
+	display_led_reg |= (temp << 7);
+	DISP_LEDS_P->OUTDR = display_led_reg;
+
 }
 
 void button_state_update()
@@ -274,9 +308,33 @@ int main()
 	SystemInit();
 	sao_init();
 
+	uint8_t update_delay = 0;
+	uint8_t disp_next_init_delay = 0;
+	uint8_t disp_next_init_cnt = 0;
+
+	display_led_reg = display_init_vals[0];
+
 	while (1)
 	{
 		Delay_Ms(50);
 		button_state_update();
+
+		update_delay++;	
+		
+
+		if (update_delay >= 5)
+		{
+			disp_next_init_delay++;
+			update_delay = 0;
+			display_leds_update();
+		}
+		
+		if (disp_next_init_delay >= DISP_UPDATE_NEXT_INIT)
+		{
+			disp_next_init_delay = 0;
+			disp_next_init_cnt++;
+			display_led_reg = display_init_vals[disp_next_init_cnt % DISP_INIT_VAL_LEN];
+		}
+
 	}
 }
